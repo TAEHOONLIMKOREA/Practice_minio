51aa23a737533efe512d3f3e0fbb4748a1e2b93c
LimTH <lth9029@gmail.com>
2023-09-11 02:07:48 +0900

    modify main.py - add code for multipart uploading

--- main.py
+++ main.py
@@ -1,49 +1,57 @@
 from minio import Minio
+import concurrent.futures
+import time
 import os
 
-minioClient = Minio('keties.iptime.org:59000',  # ip:port  or URL
+minio_client = Minio('keties.iptime.org:59000',  # ip:port  or URL
                     access_key='keti_root',
                     secret_key='madcoder',
                     secure=False)
 
 # 로컬 디렉토리 경로
-local_directory = "/path/to/local/directory"
+local_directory = "./(2023-04-28) 3DP DX 발표자료"
 
 # MinIO 버킷 이름
 minio_bucket = "your-minio-bucket"
 
+def upload_file_to_minio(file_path, minio_bucket):
+    file_name = os.path.relpath(file_path, local_directory)
+    object_name = "/" + file_name.replace("\\", "/")
+    try:
+        # 파일을 MinIO 버킷에 멀티파트 업로드 (조각 크기 : 100MB)
+        minio_client.fput_object(minio_bucket, object_name, file_path, part_size=100 * 1024 * 1024)
+        print(f"Uploaded: {object_name}")
+    except Exception as err:
+        print(f"Error uploading {object_name}: {err}")
+
 def upload_directory_to_minio(local_path, minio_bucket):
-    for root, dirs, files in os.walk(local_path):
-        for file in files:
-            file_path = os.path.join(root, file)
-            object_name = os.path.relpath(file_path, local_path)
-            try:
-                # 파일을 MinIO 버킷에 업로드
-                minioClient.fput_object(minio_bucket, object_name, file_path)
-                print(f"Uploaded: {object_name}")
-            except Exception as e:
-                print(f"Error uploading {object_name}: {e}")
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        for root, dirs, files in os.walk(local_path):
+            for file in files:
+                file_path = os.path.join(root, file)
+                executor.submit(upload_file_to_minio, file_path, minio_bucket)
+
+
+# def upload_directory_to_minio(local_path, minio_bucket):
+#     for root, dirs, files in os.walk(local_path):
+#         for file in files:
+#             file_path = os.path.join(root, file)
+#             file_name = os.path.relpath(file_path, local_path)
+#             object_name = "/" + file_name.replace("\\", "/")
+#             try:
+#                 # 파일을 MinIO 버킷에 업로드
+#                 minio_client.fput_object(minio_bucket, object_name, file_path)
+#                 print(f"Uploaded: {object_name}")
+#             except Exception as e:
+#                 print(f"Error uploading {object_name}: {e}")
 
 if __name__ == "__main__":
-    upload_directory_to_minio(local_directory, minio_bucket)
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-
-
-    found = minioClient.bucket_exists("my-bucket")
+    found = minio_client.bucket_exists(minio_bucket)
     if not found:
-        minioClient.make_bucket("my-bucket")
+        minio_client.make_bucket(minio_bucket)
     else:
-        print("Bucket 'my_bucket' already exists")
-
-    # Upload '/home/user/Photos/asiaphotos.zip' as object name
-    # 'asiaphotos-2015.zip' to bucket 'asiatrip'.
-    # minioClient.fput_object("my-bucket", "test.zip", "./asset/test.zip", )
-    # print("'./asset/test.zip' is successfully uploaded as "
-    #       "object 'test.zip' to bucket 'my_bucket'.")
-
-    minioClient.fget_object("my-bucket", "test.zip", "./asset/my-filename.zip")
-
+        print("Bucket " + minio_bucket + " already exists")
+    start_time = time.time()
+    upload_directory_to_minio(local_directory, minio_bucket)
+    print("---{}s seconds---".format(time.time()-start_time))
 
