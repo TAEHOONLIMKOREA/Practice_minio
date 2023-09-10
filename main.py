7d4ef04b02bd54da47e25959b179e82dca738004
LimTH <lth9029@gmail.com>
2023-09-11 02:07:04 +0900

    modify main.py - add code for uploading dir

--- main.py
+++ main.py
@@ -1,12 +1,36 @@
 from minio import Minio
-import urllib3
+import os
+
+minioClient = Minio('keties.iptime.org:59000',  # ip:port  or URL
+                    access_key='keti_root',
+                    secret_key='madcoder',
+                    secure=False)
+
+# 로컬 디렉토리 경로
+local_directory = "/path/to/local/directory"
+
+# MinIO 버킷 이름
+minio_bucket = "your-minio-bucket"
+
+def upload_directory_to_minio(local_path, minio_bucket):
+    for root, dirs, files in os.walk(local_path):
+        for file in files:
+            file_path = os.path.join(root, file)
+            object_name = os.path.relpath(file_path, local_path)
+            try:
+                # 파일을 MinIO 버킷에 업로드
+                minioClient.fput_object(minio_bucket, object_name, file_path)
+                print(f"Uploaded: {object_name}")
+            except Exception as e:
+                print(f"Error uploading {object_name}: {e}")
+
+if __name__ == "__main__":
+    upload_directory_to_minio(local_directory, minio_bucket)
+
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    minioClient = Minio('keties.iptime.org:59000',  # ip:port  or URL
-                        access_key='keti_root',
-                        secret_key='madcoder',
-                        secure=False)
+
 
     found = minioClient.bucket_exists("my-bucket")
     if not found:
